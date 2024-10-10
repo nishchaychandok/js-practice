2581f667f8d967ccbc6360238357ad2ba18f932e
nishchaychandok <143934476+nishchaychandok@users.noreply.github.com>
2024-10-10 13:56:02 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,41 @@
+const name = "Nishchay"
+const repocount=50
+
+//console.log(name+repocount+"  Value");
+
+
+//console.log(`hello my name is ${name} and my repo count is ${repocount}`); // This is called String interpolation 
+// better way than concatenation
+
+const gameName = new String('Nishbeast')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+
+//console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(5));
+
+//console.log(gameName.indexOf("t"));
+
+const newString = gameName.substring(0,5) // In this last value is not included meaning value on 5th positn will not be shown
+
+//console.log(newString);
+
+const anotherString = gameName.slice(-4,9) // In this the first value is not included , negative values can be used
+
+//console.log(anotherString);
+
+const newString1= "     Nishchay    "
+
+//console.log(newString1);
+//console.log(newString1.trim());
+
+
+
+
+
+
+
+
