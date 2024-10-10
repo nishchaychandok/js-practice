44988d7d4d65f05ecc674e4d8fc41c6793c9852b
nishchaychandok <143934476+nishchaychandok@users.noreply.github.com>
2024-10-10 16:41:41 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+//const score = 400
+
+//console.log(score);
+
+
+//const balance = new  Number(100)
+//console.log(balance);
+
+
+//console.log(balance.toString().length);
+//console.log(typeof(balance));
+
+//console.log(balance.toFixed(2)); // Fixes value after decimal , useful in ecommerce apps
+
+
+//const otherNumber=123.8966
+
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));// For Indian Values
+
+
+//+++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.3));
+//console.log(Math.random());
+console.log(Math.random()*10+1); // to avoid getting 0
+
