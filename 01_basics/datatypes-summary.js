210e813ab1375ee1f826e4e4aba34b958a5e085c
nishchaychandok <143934476+nishchaychandok@users.noreply.github.com>
2024-10-10 13:05:19 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+// Primitive 
+
+// 7 types : (Call BY VAlue = Copy File )  String, Numbers, Boolean, null, undefined, Symbol, BigInt
+
+
+
+
+//Reference type ( Non Primitive )
+
+// Array, Objects, Functions, 
+
+const heros = ["Flying jatt", " MARIO", "Shaktimaan"]
+
+let myObj= {
+
+    name :"NISHCHAY",
+
+    age : 21,
+
+
+}
+
+const myfunc = function(){
+    console.log("HELLO WORLD");
+    
+}
+
+
+console.log(myObj);
