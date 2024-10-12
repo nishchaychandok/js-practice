0d9ac064fca584d79b3a7fd6326d0ea8469e662e
nishchaychandok <143934476+nishchaychandok@users.noreply.github.com>
2024-10-12 05:16:26 +0000

    math update

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -28,5 +28,10 @@ const hundreds = 1000000
 //console.log(Math.abs(-4));
 //console.log(Math.round(4.3));
 //console.log(Math.random());
-console.log(Math.random()*10+1); // to avoid getting 0
+//console.log(Math.random()*(10)+1); // to avoid getting 0
 
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
