1278b62cbbef4226a0a30d8e2d8ac6537b9ba865
nishchaychandok <143934476+nishchaychandok@users.noreply.github.com>
2024-10-13 11:41:37 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,30 @@
+// array  
+
+const myArr = [0,1,2,3,4,]
+
+const myHeroes = ["Spiderman", "Batman", "Krish", "Ironman"]
+
+const myArr2 = new Array(1,2,3,4)
+ //console.log(myArr[0]);
+ 
+
+ // ARRAY METHODS
+
+ //myArr.push(6)             // Push is used to add new values
+ //console.log(myArr);
+ 
+ //myArr.pop() // Removes the last value from array...
+ 
+ 
+ //myArr.unshift(7) // to insert any value at the very begining...
+ 
+ //console.log(myArr.includes(9)); 
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
+ 
+
+
